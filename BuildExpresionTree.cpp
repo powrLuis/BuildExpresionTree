fe2f0abce70515ca402ae434cf4b95dc61b1dd5f
Luis Ronald Istaña Chipana <l.ronald.ic@outlook.com>
2021-09-15 18:46:08 -0500

    diferentes pruebas.

--- BuildExpresionTree.cpp
+++ BuildExpresionTree.cpp
@@ -68,11 +68,7 @@ struct ExpressionTree
 
 ExprNode* Parse(std::string expr)
 {
-    auto op_suma = expr.find('+');
-    auto op_resta = expr.find('-');
-    auto op_mult = expr.find('*');
-    auto op_div = expr.find('/');
-    auto pos = std::min(op_suma, op_resta);
+    auto pos = expr.find_last_of("+-");
     if (pos!=std::string::npos)
     {
         return new OpNode(expr[pos],
@@ -80,7 +76,7 @@ ExprNode* Parse(std::string expr)
             Parse(expr.substr(pos + 1, expr.size())));
     }
 
-    pos = std::min(op_mult, op_div);
+    pos = expr.find_last_of("*/");
     if (pos != std::string::npos)
     {
         return new OpNode(expr[pos],
@@ -100,17 +96,9 @@ ExpressionTree BuildExpressionTree(std::string expr)
 
 int main()
 {
-    std::string expr = "10/2";
+    //std::string expr = "10-2*5+5/2+2";//4.5
+    //std::string expr = "10-2";//8
+    std::string expr = "2*2*2/2";//4
     ExpressionTree tree = BuildExpressionTree(expr);
     std::cout <<tree.compute()<< "\n";
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
